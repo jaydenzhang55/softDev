fd658d0006ad3b6e416fc67f5106ce2341867401
projectjayden <jzhang55@stuy.edu>
2024-09-18 02:43:33 -0400

    finished 05_krewes.py

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,17 +1,30 @@
-file = open('krewes.txt','r')
+''' 
+Jayden Zhang
+J.E.D
+SoftDev
+K05 -- Reading Files, Spliting Strings, Creating Dictionaries / Using open() to open a file, split() to split a string into an array, making a list of dictionaries using append().
+2024-09-17
+time spent: 0.7 hours 
+'''
+
+import random
+
+file = open('05_bitstream\krewes.txt','r')
 
 readString = file.readline()
-
 listOfNames = readString.split('@@@') # (pd$$$devo$$$ducky, ...)
-
 list = []
 
 for name in listOfNames:
     listOfPDD = name.split('$$$') # (pd, devo, ducky)
-    print(listOfPDD)
-    list.append({'PD': listOfPDD[0], 'devo': listOfPDD[1], 'ducky': listOfPDD[2]})
+    list.append({'PD': listOfPDD[0], 'devo': listOfPDD[1], 'ducky': listOfPDD[2]}) # adds a dictonary to the list
 
-print(list)
+randomInt = random.randint(0, len(list) - 1) # chooses a random index within the list of dictionaries.
+devo = list[randomInt]["PD"] # finds the devo value of the random dictionary
+period = list[randomInt]['devo'] # finds the period value of the random dictionary
+ducky = list[randomInt]['ducky'] # finds the ducky value of the random dictionary
+
+print(devo, period, ducky) # prints a random devo, period, and ducky
 
     
     
