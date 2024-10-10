b3077ed0549b9cf642893e49dbe3874b7cf6de22
Jayden Zhang <jzhang55@stuy.edu>
2024-10-10 11:47:25 -0400

    16 flask session

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,7 +1,7 @@
 '''
-JTC - Jayden Zhang, Caden Khuu, Tim Ng
+JED - Jayden Zhang, Endrit Idrizi
 SoftDev
-K15 - Flask-Form - Learning and Experimenting with Flask GET and POST Methods.
+K16 - Flask-Form - Learning and Experimenting with Flask GET and POST Methods.
 2024-10-9
 time spent: 1 hrs
 '''
@@ -14,6 +14,7 @@ time spent: 1 hrs
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+import os
 
 #the conventional way:
 #from flask import Flask, render_template, request
@@ -25,15 +26,27 @@ def disp_loginpage():
     return render_template('login.html')
 
 
-@app.route("/auth", methods=['GET', 'POST'])
-def authenticate():
+@app.route("/signUp", methods=['GET'])
+def signUp():
     gvp = "The difference between the GET and POST methods lies in the way data is retrieved. GET checks the dictionary (args) for a key that matches the input, while POST essentially pushes your data to the servers POST dictionary (form). Both are sent as QueryStrings REQUESTS."
-    if request.method == "POST": # if the method is POST, it should push that data into the form library
-        username = request.form["username"] # POST uses request.form.
-    elif request.method == "GET": # if the method is GET, it should ask for that data from the args library.
-        username = request.args["username"] # GET uses request.form.
-    
-    return render_template('response.html', gvp=gvp, username=username, request=request.method) #response to a form submission
+    username = request.args["username"] # GET uses request.form.
+
+    app.secret_key = osrandom(32)
+
+@app.route("/logIn", methods=['GET'])
+def logIn():
+    gvp = "The difference between the GET and POST methods lies in the way data is retrieved. GET checks the dictionary (args) for a key that matches the input, while POST essentially pushes your data to the servers POST dictionary (form). Both are sent as QueryStrings REQUESTS."
+    username = request.args["username"] # GET uses request.form.
+
+    app.secret_key = osrandom(32)
+
+@app.route("/logOut", methods=['GET'])
+def logOut():
+    username = request.args["username"] # GET uses request.form.
+
+    app.secret_key = osrandom(32)
+
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
