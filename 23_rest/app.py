ac15b09db5f882fbe8aa28dda35d1e9495bf4c3e
projectjayden <jaydenz0704@gmail.com>
2024-11-21 09:19:05 -0500

    finished 23

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -1,7 +1,24 @@
-import flask, render_template, urllib.request, json
+'''
+NJ - Jayden Z., Naf M.
+SoftDev
+K23 -- Learning About and Using Rest APIs (Specifically NASA's API)
+2024-11-21
+time spent: 0.5
+'''
 
-@app.route('/', methods=)
+import urllib.request, json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    data = urllib.request.urlopen('')
-    dictionary = json.load(data.read())
-    
+    key = 'https://api.nasa.gov/planetary/apod?api_key=Ya7bZqbUfdbrFDXPnWr3sTUPj9g09UwFrBtjkcqn'
+    data = urllib.request.urlopen(key) # grabs the json file from the website.
+    dictionary = json.loads(data.read()) # translates the json file into a python dictionary.
+    hdurl = dictionary.get('hdurl') # grabs the value from the hdurl key.
+    description = dictionary.get('explanation') # grabs the value from the explanation key.
+    return render_template('main.html', photo=hdurl, description=description)
+
+if __name__ == "__main__":
+    app.debug = True 
+    app.run()
